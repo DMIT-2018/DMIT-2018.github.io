2d3a7232610f32f7347557b5d5c33598a1dcad43
Don Welch <dwelch@nait.ca>
2019-06-17 14:25:11 -0600

    activate oltp exercise

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -43,7 +43,7 @@ module.exports = {
         // page w. link to conflict resolution github repo
         'ef',
         'linq',
-        //'oltp' // split into two
+        'oltp' 
       ],
 
       '/topics/': [
